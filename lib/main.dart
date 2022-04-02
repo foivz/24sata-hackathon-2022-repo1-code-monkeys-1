0f4b459b4666cc147c6b8d524c8fbe697533fdcd
Fen1ks18 <dino14sirovica@gmail.com>
2022-04-02 11:31:32 +0200

    Wheel Scroll with image example

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:code_monkeys_project/transaction_class_folder/expense_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Analise(),
     );
   }
 }
